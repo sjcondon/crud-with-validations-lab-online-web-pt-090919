bd2198278dd80f39a2a69937f3e36cbdd8815e02
Sarah Condon <s.j.condon01@gmail.com>
2020-02-09 15:19:46 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200209015626) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "genere"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
